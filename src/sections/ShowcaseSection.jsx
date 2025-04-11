57bbe660cc1924c72205fabcb3403ca016acd7f7
Faizan <developingfaizaan@gmail.com>
2025-04-11 16:35:34 +0530

    Update README.md

--- src/sections/ShowcaseSection.jsx
+++ src/sections/ShowcaseSection.jsx
@@ -53,11 +53,11 @@ const AppShowcase = () => {
             </div>
             <div className="text-content">
               <h2>
-                On-Demand Rides Made Simple with a Powerful, User-Friendly App
-                called Ryde
+                On-Demand Exams Made Simple with a Powerful, User-Friendly Website
+                called SharEX
               </h2>
               <p className="text-white-50 md:text-xl">
-                An app built with React Native, Expo, & TailwindCSS for a fast,
+                An Website built with React Native, Expo, & TailwindCSS for a fast,
                 user-friendly experience.
               </p>
             </div>
@@ -71,14 +71,14 @@ const AppShowcase = () => {
                   alt="Library Management Platform"
                 />
               </div>
-              <h2>The Library Management Platform</h2>
+              <h2>The Hotel Management Platform</h2>
             </div>
 
             <div className="project" ref={ycDirectoryRef}>
               <div className="image-wrapper bg-[#FFE7EB]">
                 <img src="/images/project3.png" alt="YC Directory App" />
               </div>
-              <h2>YC Directory - A Startup Showcase App</h2>
+              <h2>Portfolio Builder - A Professional Showcase Website</h2>
             </div>
           </div>
         </div>
